b3b9d3b14eabd8d631602c27c143798e5e9d11d4
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-05 09:26:42 -0700

    Fix task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/** 
- *  main - The entry point of any c program   
- */
+/**
+* main - The entry point of any c program
+* Return: 0 which is the exit code for the program
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
