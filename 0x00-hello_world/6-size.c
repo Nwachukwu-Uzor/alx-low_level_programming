65291cb0c0f852d60374c5eea9e5b076cc23b17b
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-05 10:43:34 -0700

    Task 6: Write a C program that prints the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - The entry point of any c program
+* Return: 0 which is the exit code for the program
+*/
+int main(void)
+{
+printf("Size of a char: 1 byte(s)");
+printf("Size of an int: 4 byte(s)");
+printf("Size of a long int: 4 byte(s)");
+printf("Size of a long long int: 8 byte(s)");
+printf("Size of a float: 4 byte(s)");
+return (0);
+}
