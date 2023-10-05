49570c9d3df4db8f2f219b77d34658bed9fbe3f1
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-05 09:31:16 -0700

    Task 5: Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - The entry point of any c program
+* Return: 0 which is the exit code for the program
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
