0decc2fb7cde14cb61482eb9d89d029bc4d966a2
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-06 13:10:41 -0700

    Task 9: Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - Entry point
+* Return: Prints all lowercase characters from a to z
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+       putchar('0' + i);
+       putchar(',');
+       putchar(' ');
+putchar('\n');	
+return (0);
+}
