2eeeb8ba9589dc1329875e67a5e86ba8d05dca05
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-06 09:13:37 -0700

    Fix: task 1 remove extra spaces

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,10 +16,10 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 ldgt = n % 10;
 if (ldgt > 5)
-	printf("Last digit of %d is  %d and is greater than 5\n", n, ldgt);
+	printf("Last digit of %d is %d and is greater than 5\n", n, ldgt);
 else if (ldgt == 0)
-	printf("Last digit of %d is  %d and is 0\n", n, ldgt);
+	printf("Last digit of %d is %d and is 0\n", n, ldgt);
 else
-	printf("Last digit of %d is  %d and s less than 6 and not 0\n", n, ldgt);
+	printf("Last digit of %d is %d and s less than 6 and not 0\n", n, ldgt);
 return (0);
 }
