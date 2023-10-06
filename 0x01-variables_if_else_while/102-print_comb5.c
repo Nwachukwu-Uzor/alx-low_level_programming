ec1c6216a3f4f39554dfeb62cb7db2def9b6ecb3
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-06 15:46:45 -0700

    fix task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,14 +12,14 @@ int main(void)
 int i, j;
 for (i = 0; i < 100; i++)
 {
-	for (j = 0; j <= 100; j++)
+	for (j = 0; j < 100; j++)
 	{
 		putchar(i/10 + '0');
 		putchar((i%10) + '0');
 		putchar(' ');
 		putchar(j/10 + '0');
 		putchar((j%10) + '0');
-		if (i == 99 && j == 99)
+		if (i == 99 && j == 98)
 		{
 			continue;
 			
