70f7dd758b4f5ede321fae82839eae3f1c7d59ab
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-06 12:07:24 -0700

    Task 3:  alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - Entry point
+* Return: Prints all lowercase characters from a to z
+*/
+int main(void)
+{
+char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int length = strlen(alpha);
+int i;
+for (i = 0; i < length; i++)
+	putchar(alpha[i]);
+putchar('\n');
+return (0);
+}
