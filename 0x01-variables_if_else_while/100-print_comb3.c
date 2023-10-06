5a8f3c42c9754470575ad0904cf365ab496b36a7
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-06 14:00:01 -0700

    Fix task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,9 +15,12 @@ for (i = 0; i < 9; i++)
 	for (j = i + 1; j < 10; j++)
 	{
 		putchar('0' + i);
-		putchar('0' + j)
-		putchar(',');
-		putchar(' ');
+		putchar('0' + j);
+		if (i < 8 && j < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 }
 putchar('\n');
