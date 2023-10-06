7e99253a4d9e2b5abfbc30ee537a8b1d05207757
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-06 12:17:51 -0700

    Task 6: Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - Entry point
+* Return: Prints all lowercase characters from a to z
+*/
+int main(void)
+{
+for (i = 0; i < 10; i++)
+	putchar(alpha[i]);
+putchar('\n');
+return (0);
+}
