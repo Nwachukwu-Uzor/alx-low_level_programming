cc2227bb0706c0ada430b53adb7dea7be625e690
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-10 12:30:11 -0700

    Task 2: 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - A function that prints all lowercase alphabets
+* Return: Prints all lowercase characters from a to z
+*/
+void print_alphabet_x10(void)
+{
+	char alp;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alp = 'a'; alp <= 'z'; alp++)
+		{
+			_putchar(alp);
+		}
+		_printchar('\n');
+	}
+}
