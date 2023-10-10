3284e3f105bdc981ee8b061d7986565cc1c0eb32
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-10 11:56:39 -0700

    Fix task 0 3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,13 @@
 */
 int main(void)
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('a');
+_putchar('r';
+_putchar('\n');
 return (0);
 }
