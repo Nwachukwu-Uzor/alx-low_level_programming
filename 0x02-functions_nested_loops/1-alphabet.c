94088dd27776f7173b2762c50d4588d1118c87c2
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-10 12:08:30 -0700

    Task 1:  I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* Return: Prints all lowercase characters from a to z
+*/
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		_putchar('r');
+		_putchar('\n');
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef PUT_CHAR
 #define PUT_CHAR
 int _putchar(char c);
+void print_alphabet(void);
 #endif
