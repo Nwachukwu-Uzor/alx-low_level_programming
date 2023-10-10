9b0c7a735946b4df489ba24dffaa811f433ff709
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-10 13:56:01 -0700

    Fix task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,13 +17,13 @@ void jack_bauer(void)
 			{
 				_putchar('0');
 			}
-			_putchar(hr + '0');
+			_putchar(hr);
 			_putchar(':');
 			if (min < 10)
 			{
 				_putchar('0');
 			}
-			_putchar(min + '0');
+			_putchar(min);
 			_putchar('\n');
 		}
 	}
