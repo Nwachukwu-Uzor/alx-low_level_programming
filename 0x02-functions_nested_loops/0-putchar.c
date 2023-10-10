fae97263885d4c83197604f536f5dd4cf7478433
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-10 06:33:21 -0700

    Task 0:  _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - Entry point
+* Return: Prints all lowercase characters from a to z
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
