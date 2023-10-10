a1002888451cc886d8daf211b1bf777465d122eb
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-10 15:23:46 -0700

    Task 11: 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio.lib>
+
+/**
+* print_to_98 -  98 Battery Street, the OG
+* @n: the integer parameter
+* Return: void
+*/
+
+void print_to_98(int n)
+{
+	int temp;
+
+	temp = n;
+	while (temp <= 98)
+	{
+		printf("%d", temp);
+		temp++;
+	}
+}
