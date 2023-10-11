75d3389060f297270ffbe1fda9b7b967c2c4dbde
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-10 17:13:09 -0700

    Task 15 Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - write the first 50 fibonacci numbers
+* Return: void
+*/
+
+int main(void)
+{
+	int i;
+	long a, b, next, sum;
+
+	a = 1;
+	b = 2;
+	next = 0;
+	sum = 2;
+	printf("%ld, %ld", a, b);
+	while (sum < 4000000)
+	{
+		next = a + b;
+		if ((next % 2) == 0)
+		{
+			sum += next;
+		}
+		a = b;
+		b = next;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
