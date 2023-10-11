8deef6c901e748d09585c738168cdfbedd3285bb
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-11 10:07:27 -0700

    Task 0:  Multiple mains

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - write the first 50 fibonacci numbers
+* Return: void
+*/
+
+int main(void)
+{
+	int i;
+	long a, b, next;
+
+	a = 1;
+	b = 2;
+
+	printf("%ld, %ld", a, b);
+	for (i = 2; i < 50; i++)
+	{
+		next = a + b;
+		printf(", %ld", next);
+		a = b;
+		b = next;
+	}
+	printf("\n");
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,4 @@
+#ifndef DEBUGGING_HEADER_FILE
+#define DEBUGGING_HEADER_FILE
+void postitive_or_negative(int);
+#endif
