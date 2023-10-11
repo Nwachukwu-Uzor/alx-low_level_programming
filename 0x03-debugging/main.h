7a7957eba2440b81ded7385643b22fe933625fc9
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-11 10:11:04 -0700

    Fix task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,8 @@
-#ifndef DEBUGGING_HEADER_FILE
-#define DEBUGGING_HEADER_FILE
-void postitive_or_negative(int);
-#endif
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
