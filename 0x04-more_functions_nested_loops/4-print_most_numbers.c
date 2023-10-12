8b4c5396cabab3e9f7f04e827d940972c7668318
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-12 06:36:40 -0700

    Fix task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_most_numbers -  a function that prints the numbers, from 0 to 9, followed by a new line
+* print_most_numbers -  a function that prints the numbers
 * Return: print all number from 0 to 9, skipping 2 and 4
 */
 
