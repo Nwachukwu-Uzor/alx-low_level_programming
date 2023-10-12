b4b5acb87916958cd9311fc8707036bf1dd2ad22
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-12 08:25:04 -0700

    Fix task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,7 +8,7 @@
 
 void print_square(int size)
 {
-	int i;
+	int i, j;
 
 	if (size <= 0)
 	{
@@ -18,7 +18,7 @@ void print_square(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
+		for (j = 0; j < size; j++)
 		{
 			_putchar('#');
 		}
