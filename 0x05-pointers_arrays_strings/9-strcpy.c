d2359acdf6863daa0a395921f9033e02419754c9
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-16 14:49:28 -0700

    Task 9: strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strcpy - opies the string pointed to by src
+* @dest: the destination
+* @src: the source
+* Return: the pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\n'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
+}
