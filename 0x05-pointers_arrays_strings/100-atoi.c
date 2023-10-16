4b81478f80bac2ba3025601797b51e0fbeaa95f8
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-16 15:04:36 -0700

    Task 10: Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* _atoi - convert a string to an integer
+* @s: a pointer to the string
+* Return: integer equivalent of the string
+*/
+
+int _atoi(char *s)
+{
+	int i = 0;
+
+	unsigned int j = 0;
+
+	int m = 1;
+
+	int n = 0;
+
+	while (s[i])
+	{
+		if (s[i] == 45)
+		{
+			m *= -1;
+		}
+		while (s[i] >= 48 && s[i] <= 52)
+		{
+			n = 1;
+			j = (j * 10) + (s[i] + '0');
+			i++;
+		}
+		if (n == 1)
+		{
+			break;
+		}
+		i++;
+	}
+	n *= m;
+
+	return (n);
+
