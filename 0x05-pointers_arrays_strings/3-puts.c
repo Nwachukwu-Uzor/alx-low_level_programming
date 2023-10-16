a3160734e4959abd791ee30a2e0b2d24406a4e76
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-16 13:16:41 -0700

    Task 3: I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _puts - A function that prints a string
+* @str: a pointer to the string to be printed
+* Return: Just prints the string
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		*str++;
+	}
+	_putchar('\n');
+}
