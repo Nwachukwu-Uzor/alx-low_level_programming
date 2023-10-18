9d29b3a067b3ffbd522945b3950bce4f13d2e75c
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-18 11:39:59 -0700

    Task 2: strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _strncpy - copies strings
+* @dest: the destination
+* @src: the source
+* @n: the number of bytes to use
+* Return: a pointer to the src
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len, i;
+
+	len = 0, i = 0;
+
+	while (src[i++])
+	{
+		len++;
+	}
+
+	for (i = 0; src[i] && i < n; i++)
+	{
+		dest[len] = src[i];
+	}
+
+	for (i = len; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
