4f9014085d2a23ab15e9fffef3d2a042fc8ebe6c
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-23 21:02:23 -0700

    Task 0: memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _memset - memory with a constant byte.
+* @s: memory area
+* @b: constant byte
+* @n: the first n bytes
+* Return: a pointer to new memory location
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsignd int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+char *_memset(char *s, char b, unsigned int n);
+#endif
