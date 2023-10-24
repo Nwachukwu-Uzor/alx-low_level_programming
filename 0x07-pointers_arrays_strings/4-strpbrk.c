43bae0c5a62a4f51bcefb2f633d8692d74f52aeb
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-23 21:55:05 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strpbrk - fxn name
+* @s: param 1
+* @accept: param 2
+* Return: val
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return ('\0');
+}
