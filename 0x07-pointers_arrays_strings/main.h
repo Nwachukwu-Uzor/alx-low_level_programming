de269e62ce171a48db91a76f622d93d8025d5568
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-23 21:24:04 -0700

    Fix task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_HEADER
 #define MAIN_HEADER
+#include <string.h>
+#include <stdio.h>
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
