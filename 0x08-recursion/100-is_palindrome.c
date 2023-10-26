b034640d39d602e0b8c59e337e1f221f77ae969b
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-26 00:30:34 -0700

    Task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+int _compare_string(char *s, int i, int j);
+
+/**
+* is_palindrome - checks if a string is a palindrome
+* @s: the string to check
+* Return: 1 if string is palindrome else 0
+*/
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+	{
+		return (1);
+	}
+
+	return (_compare_string(s, _strlen_recursion(s) - 1, 0));
+}
+
+/**
+* _compare_string - compare string characters
+* @s: the string to compare
+* @i: the largest iterator
+* @j: the smallest iterator
+* Return: 1 if it's a palindrome else 0
+*/
+
+int _compare_string(char *s, int i, int j)
+{
+	if ((s + j) == *(s + i))
+	{
+		if (i == j || j == i + 1)
+		{
+			return (1);
+		}
+		return (0 + _compare_string(s, i - 1, j + 1));
+	}
+	return (0);
+}
