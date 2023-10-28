076ecd8722b9113c1782bf9418cb6ff712e7d61d
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-10-28 05:53:10 -0700

    Task 3:  Neither irony nor sarcasm is argumen

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - prints all arguments it receives
+* @argc: the number of arguments
+* @argv: the argument vector
+* Return: always returns 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", argv[1] * argv[2]);
+	return (0);
+}
