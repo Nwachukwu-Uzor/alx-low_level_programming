c03a1f459c5213f90cf7c00bb3e41898ccddd176
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-11-02 13:42:26 -0700

    Fix task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -41,7 +41,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	for (i = 0; i < old_size && i < new_size; i++)
 	{
-		p = ((char *) ptr)[i];
+		p[i] = ((char *) ptr)[i];
 	}
 	free(ptr);
 	return (p);
