f508685e8428dec8b49fb2b0029a70961e8e0b47
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-11-02 12:45:28 -0700

    Fix Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		m = i + j;
 	else
 		m = i + n;
-	s = malloc(sizeof(char) + m + 1);
+	s = malloc(sizeof(char) * m + 1);
 	if (s == NULL)
 		return (NULL);
 	j = 0;
