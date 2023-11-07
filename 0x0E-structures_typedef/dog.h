7468e3672e2529e26a95b74abfd114d8b0efe2d8
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-11-07 01:53:26 -0800

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+/**
+* struct dog - the dog struct
+* @name: the name of the dog
+* @age: the age of the dog
+* @owner: the owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
