33d033118d1d8274f2eae4e5ed7617cb72cb645a
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-11-08 07:06:46 -0800

    Fix task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,14 +11,15 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i;
+
 	if (array == NULL || action == NULL)
 	{
 		return;
 	}
 
-	while (size-- > 0)
+	for (i = 0; i < size; i++)
 	{
-		action(array);
-		array++;
+		action(array[i]);
 	}
 }
