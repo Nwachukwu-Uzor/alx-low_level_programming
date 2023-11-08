a1a41e1873e6050d1c020e2cd5641d0f593562d2
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-11-08 07:44:12 -0800

    Fix task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -19,10 +19,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array(i)))
+		if (cmp(array(i)) != 0)
 		{
 			return (i);
 		}
 	}
+
 	return (-1);
 }
