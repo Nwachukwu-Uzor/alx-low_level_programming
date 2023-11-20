6f39ce16ef59f3ae4babd2432d0623df79f802f6
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-11-20 13:29:34 -0800

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *free_listint - a function that frees a listint_t list.
+ *@head: pointer to the head node
+ *Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
