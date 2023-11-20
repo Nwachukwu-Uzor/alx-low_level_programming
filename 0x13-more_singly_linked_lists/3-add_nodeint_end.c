c122b2885c11ff79853100adcdd3af874c1835c4
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-11-20 13:26:30 -0800

    fix task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *holder;
 	listint_t *tmp = *head;
 
-	new = malloc(sizeof(listint_t));
+	holder = malloc(sizeof(listint_t));
 	if (!holder)
 		return (NULL);
 
