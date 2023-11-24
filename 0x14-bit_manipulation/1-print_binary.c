592c096b647820294621f8212c4803852031af5a
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-11-24 13:56:54 -0800

    Fix task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,23 +1,29 @@
 #include "main.h"
 
-int _raise_to_power(unsigned int base, unsigned int power);
+void _print_binary(unsigned long int value);
 
 /**
- *_raise_to_power - raises a number to a value
- *@base: the base
- *@power: the power
- *Return: value of (base ^ power)
+ * _print_binary - prints a binary number recursively
+ * @value: the value to print
  */
 
-unsigned long int _raise_to_power(unsigned int base, unsigned int power)
+void _print_binary(unsigned long int value)
 {
-	unsigned long int n;
-	unsigned int counter;
+	if (value == 0)
+	{
+		return;
+	}
 
-	n = 1;
-	for (counter = 1; counter <= power; counter++)
-		n *= base;
-	return (n);
+	_print_binary(value >> 1);
+	if ((value & 1) == 1)
+	{
+		_putchar('1')
+	}
+
+	if ((value & 1) == 0)
+	{
+		_putchar('0')
+	}
 }
 
 /**
@@ -28,24 +34,11 @@ unsigned long int _raise_to_power(unsigned int base, unsigned int power)
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int div, check;
-	char flag;
-
-	flag = 0;
-	div = _raise_to_power(2, (sizeof(unsigned long int) * 8) - 1);
-	while (div != 0)
+	if (n == 0)
 	{
-		check = n & div;
-		if (check == div)
-		{
-			flag = 1;
-			_putchar('1');
-		}
-		else if (flag == 1 || div == 1)
-		{
-			_putchar('0');
-		}
-
-		div >>= 1;
+		_putchar('0');
+	} else
+	{
+		_print_binary(n);
 	}
 }
