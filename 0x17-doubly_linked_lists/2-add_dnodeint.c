2536faf278f857bfc807bef068bbe0ec2edb4b91
Nwachukwu-Uzor <uzor1997@gmail.com>
2024-01-11 11:54:06 -0800

    Fix 2. Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *added_node;
 	dlistint_t *h;
 
-	new = malloc(sizeof(dlistint_t));
+	added_node = malloc(sizeof(dlistint_t));
 	if (added_node == NULL)
 		return (NULL);
 
