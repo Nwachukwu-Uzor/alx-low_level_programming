bfb12dd9f14e0d5b2f9efded5bb171426614d31f
Nwachukwu-Uzor <uzor1997@gmail.com>
2024-01-11 11:52:01 -0800

    Task 2. Add node

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - appends a new nod to a dll
+ * @head: head node
+ * @n: the value at new nod
+ * Return: the address of added node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *added_node;
+	dlistint_t *h;
+
+	new = malloc(sizeof(dlistint_t));
+	if (added_node == NULL)
+		return (NULL);
+
+	added_node->n = n;
+	added_node->prev = NULL;
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
+
+	added_node->next = h;
+
+	if (h != NULL)
+		h->prev = added_node;
+
+	*head = added_node;
+
+	return (added_node);
+}
